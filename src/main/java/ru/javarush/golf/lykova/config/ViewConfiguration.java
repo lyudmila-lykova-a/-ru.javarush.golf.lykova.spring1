1e129482f1b8ad2e337661a687a8064bc035ffae
Lyudmila Lykova <lykovalud@rambler.ru>
2023-03-31 23:35:55 +0300

    view configuration

---
+++ src/main/java/ru/javarush/golf/lykova/config/ViewConfiguration.java
@@ -0,0 +1,51 @@
+package ru.javarush.golf.lykova.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.context.WebApplicationContext;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+@Configuration
+public class ViewConfiguration {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Bean
+    public ServletContextTemplateResolver templateResolver() {
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(webApplicationContext.getServletContext());
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setTemplateEngineMessageSource(messageSource());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver() {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        viewResolver.setOrder(1);
+        return viewResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+}
--- src/main/java/ru/javarush/golf/lykova/controller/DemoController.java
+++ src/main/java/ru/javarush/golf/lykova/controller/DemoController.java
@@ -1,15 +1,10 @@
 package ru.javarush.golf.lykova.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import ru.javarush.golf.lykova.entity.TaskEntity;
 import ru.javarush.golf.lykova.service.DemoService;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("api")
@@ -18,10 +13,9 @@ public class DemoController {
     @Autowired
     private DemoService demoService;
 
-    @RequestMapping(value = "demo", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public List<TaskEntity> findAllTasks() {
-        return demoService.findAllTasks();
+    @RequestMapping(value = "demo", method = RequestMethod.GET)
+    public String findAllTasks() {
+        return "demo";
     }
 
 }
